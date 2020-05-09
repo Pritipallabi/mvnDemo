710d31b591c1bdd0a9f5f11e626da4533f3ed62f
PritiPallabi <pritipalabi@gmail.com>
2020-05-10 00:35:45 +0530

    Added one fle test case2 and AmazonTest modified

--- src/test/java/Mydemo/AmazonTest.java
+++ src/test/java/Mydemo/AmazonTest.java
@@ -1,2 +1,9 @@
 //this is edit test case.
 //This is test case amazon login
+
+
+
+//This is modified added new code
+//This is modified added new code
+//This is modified added new code
+//This is modified added new code
